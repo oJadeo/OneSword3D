9503e97ea6707fdf37f157ad10749bf908f4ec9a
oJadeo <jade.jiravate@gmail.com>
2023-02-16 20:03:25 +0700

    Fix Camera

--- Prototype/Player_Character/GD_PlayerCharacter.gd
+++ Prototype/Player_Character/GD_PlayerCharacter.gd
@@ -1,41 +1,17 @@
 extends CharacterBody3D
 
-#Exploring variable
-const SPEED = 5 #150 for 2 tile gap Hrozon vetical 3 tile for diagonal
-const JUMP_VELOCITY = 3.5
-var direction 
-#Dash variable
-var DASH_SPEED = 20
-var dash_dur = 0.1
-const MAX_CHARGE = 3
-var dash_charge = 3
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-# Wallrun variable
-var wall_normal
-var is_wall_runable = false
-@onready var wall_run_timer = $Wallrun_Timer
-#Attack variable
-var attacking = false
-var input_combo = 1
-var current_combo = 1
-
-#Deflect variable
-var perfect_deflect = false
-var deflecting = false
-var knockback = false
-var last_direction = Vector2.ZERO
-const MAX_BLOCK_BAR = 100
-var blockBar = 100
-
-var begin_regen = false
-var is_regen = false
-
 #signals
 signal Blockbar_changed
 signal DashCharge_changed
 
-
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+func _physics_process(delta):
+	handle_input()
+	handle_move(delta)
+	handle_atk()
+	handle_animation()
 
 # input variable
 var input_frame = {
@@ -47,25 +23,6 @@ var input_frame = {
 	"rotate_cw" : false,
 	"rotate_ccw" : false,
 	}
-
-#Animation variable
-@onready var animationTree = $AnimationTree
-@onready var animationState = animationTree.get("parameters/playback")
-@onready var root = $".."
-#@onready var dash = $Dash
-@onready var playerCamera = $SpringArm3d/Camera3d
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-func _physics_process(delta):
-	handle_input()
-	handle_move(delta)
-	handle_atk()
-	handle_animation()
-		
-
 #take all input into input dict
 func handle_input():
 	input_frame["direction"] = Vector2(Input.get_axis("Move_Left", "Move_Right"),Input.get_axis("Move_Up", "Move_Down")) 
@@ -79,7 +36,7 @@ func handle_input():
 		animationTree.set("parameters/Attack_3/blend_position",input_frame["direction"])
 		animationTree.set("parameters/Dash/blend_position",input_frame["direction"])
 	if input_frame["direction"] != Vector2.ZERO:
-		last_direction = input_frame["direction"]
+		last_direction = (transform.basis * Vector3(input_frame["direction"] .x,0, input_frame["direction"] .y)).normalized()
 	input_frame["attack"] = Input.is_action_just_pressed("Attack")
 	input_frame["deflect"] = Input.is_action_pressed("Deflect")
 	input_frame["rotate_cw"] = Input.is_action_just_pressed("Rotate_CW")
@@ -96,7 +53,12 @@ func handle_input():
 	if input_frame["rotate_ccw"]:
 		rotate(Vector3(0,1,0),deg_to_rad(45))
 
-
+#Exploring variable
+const SPEED = 5 #150 for 2 tile gap Hrozon vetical 3 tile for diagonal
+const JUMP_VELOCITY = 3.5
+var direction 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func handle_move(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -118,8 +80,6 @@ func handle_move(delta):
 		else:
 			pass
 			#animationState.travel("Jump")
-	else:
-		is_wall_runable = false
 #	if (dash.dashFinished):
 #		playerCamera.current = true
 #	if Input.is_action_just_pressed("Dash") && dash.can_dash && !dash.is_dashing() && dash_charge != 0:
@@ -143,6 +103,10 @@ func handle_move(delta):
 		velocity.y = JUMP_VELOCITY
 		is_wall_runable = true
 		wall_run_timer.start()
+	if input_frame["jump"] and is_wall_running:
+		velocity += wall_normal.get_normal(0)*SPEED
+		velocity.y = JUMP_VELOCITY
+		reset_wall_run()
 	if knockback :
 		velocity = -Vector3(last_direction.dot(cam_dir[0]),0,last_direction.dot(cam_dir[1]))*SPEED*2.5
 		move_and_slide()
@@ -150,61 +114,73 @@ func handle_move(delta):
 		knockback = false
 #	if softCollision.is_colliding():
 #		velocity += softCollision.get_push_vector() *_delta* 200
-
 	if not is_on_floor() and direction and is_on_wall() and is_wall_runable:
 		wall_run()
-		
+	if (is_on_floor() or input_frame["direction"] == Vector2.ZERO) and is_wall_running:
+		print(is_on_floor())
+		print(input_frame["direction"])
+		reset_wall_run()
+	if is_wall_running and is_wall_runable:
+		velocity -= wall_normal.get_normal(0)*SPEED
+		velocity.y = 0
 	if not attacking and not deflecting:
 		move_and_slide()
 
+#Dash variable
+var DASH_SPEED = 20
+var dash_dur = 0.1
+const MAX_CHARGE = 3
+var dash_charge = 3
+# Wallrun variable
+var wall_normal
+var is_wall_running = false
+var is_wall_runable = false
+@onready var wall_run_timer = $Wallrun_Timer
 func wall_run():
 	wall_normal = get_slide_collision(0)
 	await get_tree().create_timer(0.2).timeout
-	velocity.y = 0
-	velocity -= wall_normal.get_normal(0)*SPEED
+	is_wall_running = true
+func reset_wall_run():
+	is_wall_runable = false
+	is_wall_running = false
+func _on_wallrun_timer_timeout():
+	reset_wall_run()
 
+#Attack variable
+var attacking = false
+func handle_atk():
+	if input_frame["attack"]:
+		attacking = true
+func reset_attack():
+	attacking = false
+func _on_hitbox_area_entered(area):
+	if area.name == "HurtBox":
+		print("Player:hit enemy")
+	elif area.name == "HitBox":
+		print("Player:parry success")
+
+#Deflect variable
+var perfect_deflect = false
+var deflecting = false
+var knockback = false
+var last_direction = Vector2.ZERO
+const MAX_BLOCK_BAR = 100
+var blockBar = 100
+var begin_regen = false
+var is_regen = false
 func on_deflect_animation_end():	
 	perfect_deflect = false
 
-func handle_atk():
-	if input_frame["attack"]:
-		if attacking:
-			match input_combo:
-				1:
-					input_combo = 2
-				2:
-					input_combo = 3
-				3:
-					input_combo = 4
-		else:
-			attacking = true
-func next_attack():
-	match current_combo:
-		1:
-			if input_combo > 1:
-				current_combo = 2
-		2:
-			if input_combo > 2:
-				current_combo = 3
-func reset_attack():
-	attacking = false
-	input_combo = 1
-	current_combo = 1
-	
+#Animation variable
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+@onready var root = $".."
+#@onready var dash = $Dash
+@onready var playerCamera = $SpringArm3d/Camera3d
 func handle_animation():
 	if input_frame["direction"] == Vector2.ZERO and not attacking and not deflecting:
 		animationState.travel("Idle")
 	if attacking:
-		match current_combo:
-			1:
-				animationState.travel("Attack_1")
-			2:
-				animationState.travel("Attack_2")
-			3:
-				animationState.travel("Attack_3")
+		animationState.travel("Attack_1")
 	if (input_frame["deflect"]):	
 		animationState.travel("Block")
-
-
-func _on_wallrun_timer_timeout():
-	is_wall_runable = false
