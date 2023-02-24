73404253cdcd6ea5689a4dfc25e5628b7ee607a1
NAEM <tonnam_t@windowslive.com>
2023-02-24 22:32:21 +0700

    unfinished tuutorial map/fix cam rotation

--- Level/Testing/GD_rotate_camera.gd
+++ Level/Testing/GD_rotate_camera.gd
@@ -2,27 +2,16 @@ extends Area3D
 
 
 # Called when the node enters the scene tree for the first time.
-@export var angle = -90
-var activate = false
-var turn = false
+@export var angle = 0
 func _rotate_camera_on_body_entered(body):
-	#print("body: ",body.name)
-	if (body.name == "Player_Character" and not activate) :
-		
-		if not turn:
-			print("turn")
+	print("body: ",body.name)
+	if (body.name == "Player_Character") :
 			await get_tree().create_timer(0.05).timeout
 			#body.rotate(Vector3(0,1,0),deg_to_rad(angle))
 			body.set_rotation(Vector3(0,deg_to_rad(angle),0))
-			turn = true
-		else:
-			print("unturn")
-			await get_tree().create_timer(0.07).timeout
-			body.rotate(Vector3(0,1,0),deg_to_rad(-angle))
-			turn = false
-		activate = true
+
 
 
 func _rotate_camera_on_body_exited(body):
-	print("Player Exited")
-	activate = false
+	#print("Player Exited")
+	pass
