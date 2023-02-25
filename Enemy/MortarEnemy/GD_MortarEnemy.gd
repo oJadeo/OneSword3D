9eead6f9edcf3fe7841303b0395f522d651f319a
ChawinGowanit <chawingowanit@gmail.com>
2023-02-25 12:22:01 +0700

    add mortar bomb

--- Enemy/MortarEnemy/GD_MortarEnemy.gd
+++ Enemy/MortarEnemy/GD_MortarEnemy.gd
@@ -4,12 +4,18 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var ammo = 3
 var player
 var reloading = false
+var new_bomb
 @onready var attackTimer = $AttackTimer
 @onready var reloadTimer = $ReloadTimer
+@onready var bomb = preload("res://Enemy/MortarEnemy/S_Bomb.tscn")
+@onready var sprtie = $Sprite3d
 func _ready():
 	attackTimer.wait_time = 0.25
 func _physics_process(delta):
-
+	if reloading :
+		sprtie.modulate = Color(0,1,0)
+	else:
+		sprtie.modulate = Color(0,0,0)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	move_and_slide()
@@ -25,9 +31,20 @@ func _on_player_detection_body_entered(body):
 func _on_attack_timer_timeout():
 	attackTimer.stop()
 	if ammo > 0 and not reloading:
-		print("shoot!",player.global_position)
+		new_bomb = bomb.instantiate()
+		get_owner().add_child(new_bomb)
+		new_bomb.init(player.global_position,global_position)
 		attackTimer.start()
 		ammo -= 1
 	else:
 		reloading = true
+		reloadTimer.start()
 		print("start_reload")
+
+
+func _on_reload_timer_timeout():
+	print("finish_reload")
+	reloadTimer.stop()
+	attackTimer.start()
+	ammo = 3
+	reloading = false
