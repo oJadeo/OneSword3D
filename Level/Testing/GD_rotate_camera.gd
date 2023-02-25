f002a0bb6fff7f8d7a9f3f9c3618244d20d5cac8
NAEM <tonnam_t@windowslive.com>
2023-02-25 12:46:02 +0700

    prototype tutorial map/ delayed option on camera rotation

--- Level/Testing/GD_rotate_camera.gd
+++ Level/Testing/GD_rotate_camera.gd
@@ -3,10 +3,12 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 @export var angle = 0
+@export var delay = false
 func _rotate_camera_on_body_entered(body):
-	print("body: ",body.name)
+	#print("body: ",body.name)
 	if (body.name == "Player_Character") :
-			await get_tree().create_timer(0.05).timeout
+			if delay:
+				await get_tree().create_timer(0.5).timeout
 			#body.rotate(Vector3(0,1,0),deg_to_rad(angle))
 			body.set_rotation(Vector3(0,deg_to_rad(angle),0))
 
