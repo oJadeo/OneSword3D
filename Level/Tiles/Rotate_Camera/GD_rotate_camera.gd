98b25365c668f4471ae6360aac3663af03aede04
NAEM <tonnam_t@windowslive.com>
2023-03-05 18:06:10 +0700

    Finished Tutorial as design 05/03/2023

--- Level/Tiles/Rotate_Camera/GD_rotate_camera.gd
+++ Level/Tiles/Rotate_Camera/GD_rotate_camera.gd
@@ -8,7 +8,7 @@ func _rotate_camera_on_body_entered(body):
 	#print("body: ",body.name)
 	if (body.name == "Player_Character") :
 			if delay:
-				await get_tree().create_timer(0.5).timeout
+				await get_tree().create_timer(0.25).timeout
 			#body.rotate(Vector3(0,1,0),deg_to_rad(angle))
 			body.set_rotation(Vector3(0,deg_to_rad(angle),0))
 			get_tree().call_group("Enemy","set_rotation",Vector3(0,deg_to_rad(angle),0))
