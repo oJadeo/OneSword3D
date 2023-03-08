c1662735857125f81af7acaaa0308603fa5d28a8
ChawinGowanit <chawingowanit@gmail.com>
2023-03-08 16:43:28 +0700

    fix dummy

--- Enemy/Dummy/Rangedummy/GD_RangeDummy.gd
+++ Enemy/Dummy/Rangedummy/GD_RangeDummy.gd
@@ -9,6 +9,7 @@ extends CharacterBody3D
 enum {
 	IDLE,
 	ATTACK,
+	COOLDOWN,
 }
 
 var new_bullet
@@ -16,6 +17,7 @@ var player = null
 var direction = Vector3.ZERO
 var health = 3
 var state = IDLE
+var regen = false
 
 func _ready():
 	animationState.travel("Idle")
@@ -24,18 +26,20 @@ func _physics_process(delta):
 		direction = Global.cal_sprite_direction((player.global_position - global_position).normalized())
 	match state :
 		IDLE: 
-			animationState.travel("Idle")
+			pass
 		ATTACK:
 			animationTree.set("parameters/Attack/blend_position",direction)
+		COOLDOWN:
+			animationState.travel("Cooldown")
+			
 
 func spawn_bellet():
-	new_bullet = bullet.instantiate()
-	var bulletDirection_x = player.get_global_position().x - get_global_position().x
-	var bulletDirection_z = player.get_global_position().z - get_global_position().z
-	add_child(new_bullet)
-	print(player.global_position)
-	print(new_bullet.global_position)
-	new_bullet.init(bulletDirection_x,bulletDirection_z,0,0,2)
+	if player != null :
+		new_bullet = bullet.instantiate()
+		var bulletDirection_x = player.get_global_position().x - get_global_position().x
+		var bulletDirection_z = player.get_global_position().z - get_global_position().z
+		add_child(new_bullet)
+		new_bullet.init(bulletDirection_x,bulletDirection_z,0,0,2)
 
 func attack_fin() :
 	animationState.travel("Idle")
@@ -47,27 +51,35 @@ func attack_fin() :
 	
 
 func _on_hurtbox_area_entered(area):
+	print(health)
 	if area.name == "Hitbox":
 		if health > 1:
 			health -= 1
 			animationState.travel("Take_damage")
 			animationTree.set("parameters/Take_damage/blend_position",direction)
 			await get_tree().create_timer(1).timeout
-			if state == ATTACK :
+			if player != null :
 				animationState.travel("Attack")
 			else:
+			
 				animationState.travel("Idle")
-				
 		else:
 			animationState.travel("Death")
-			await get_tree().create_timer(3).timeout
-			animationState.travel("Regen")
-			health = 3
 
+func death_fin():
+	state = COOLDOWN
+
+func cooldown_fin():
+	state = IDLE
+	animationState.travel("Regen")
+	
 func regen_fin():
-	if state == ATTACK :
+	health = 3
+	if player != null :
+		state = ATTACK
 		animationState.travel("Attack")
 	else:
+		state = IDLE
 		animationState.travel("Idle")
 
 func _on_player_detection_body_entered(body):
