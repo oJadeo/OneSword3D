c36ace62892df28462cf6688034582833baa5d67
ChawinGowanit <chawingowanit@gmail.com>
2023-03-12 13:04:49 +0700

    menu support controller

--- Menu/GD_Level_Select.gd
+++ Menu/GD_Level_Select.gd
@@ -1,6 +1,24 @@
 extends CanvasLayer
 
+var current = 0
+@onready var buttonList = $ButtonList
 
+func _ready():
+	buttonList.get_children()[current].grab_focus()
+
+func _process(delta):
+	if Input.is_action_just_pressed("Move_Down") or Input.is_action_just_pressed("Move_Up") :
+		if Input.is_action_just_pressed("Move_Down") :
+			current += 1
+		elif  Input.is_action_just_pressed("Move_Up") :
+			current -= 1
+		current = current%5
+		buttonList.get_children()[current].grab_focus()
+		
+	if Input.is_action_just_pressed("Menu_Accept") :
+		buttonList.get_children()[current].emit_signal("pressed")
+	if Input.is_action_just_pressed("Menu_Back"):
+		buttonList.get_children()[4].emit_signal("pressed")
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Menu/S_Main_Menu.tscn")
@@ -8,3 +26,12 @@ func _on_back_pressed():
 
 func _on_tutorial_pressed():
 	get_tree().change_scene_to_file("res://Level/Level_Scene/S_tutorial_2.tscn")
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://Level/Level_Scene/S_level1_A.tscn")
+
+func _on_level_2_pressed():
+	get_tree().change_scene_to_file("res://Level/Level_Scene/S_Level_2.tscn")
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://Level/Level_Scene/S_Level_3_A.tscn")
