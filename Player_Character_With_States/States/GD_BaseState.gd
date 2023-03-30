a602928c200741290c8622d377dd69f4dcdf5676
oJadeo <jade.jiravate@gmail.com>
2023-03-30 14:37:25 +0700

    Dialogue

---
+++ Player_Character_With_States/States/GD_BaseState.gd
@@ -0,0 +1,31 @@
+extends Node
+
+enum State{
+	Null,
+	Idle,
+	Run,
+	Jump,
+	Fall,
+	WallRun,
+	WallClimb,
+	Ledge
+}
+
+@export var animation_name:String
+
+var player:PlayerCharacterWithStates
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func input(event: InputEvent) -> State:
+	return State.Null
+
+func process(delta: float) -> State:
+	return State.Null
+	
+func physics_process(delta: float) -> State:
+	return State.Null
