b7c72c60fc9f94e8eb09c6a6c5623e7cb0ecf4b8
oJadeo <jade.jiravate@gmail.com>
2023-04-10 02:58:59 +0700

    Aim Assist With Cast

--- Player_Character/GD_AimAssist.gd
+++ Player_Character/GD_AimAssist.gd
@@ -3,7 +3,7 @@ extends Area3D
 @export var assist_angle:float = 90
 @export var y_diff:float = 0.25
 @onready var camera = $"../../Camera3d"
-
+@onready var assist_ray = $"../HookRay"
 var view_size:Vector2 = Vector2(640,360)
 var target_range:float = 100
 var target_list:Array = []
@@ -24,12 +24,16 @@ func get_assist(input_angle:Vector2) -> Vector3:
 	for target in target_list:
 		var target_angle = (camera.unproject_position(target.global_position) - view_size/2).normalized()
 		var distance_vector = target.global_position- global_position
-		if distance_vector.y <= y_diff:
-			if abs(rad_to_deg(input_angle.angle_to(target_angle))) <= assist_angle:
-				if distance_vector.length() < target_range:
-					target_range = distance_vector.length()
-					output_direction = distance_vector.normalized()
-	
+		assist_ray.set_target_position(distance_vector)
+		assist_ray.force_raycast_update()
+		var is_assist_collided = assist_ray.get_collider()
+		if is_assist_collided and is_assist_collided.is_in_group('target'):
+			if distance_vector.y <= y_diff:
+				if abs(rad_to_deg(input_angle.angle_to(target_angle))) <= assist_angle:
+					if distance_vector.length() < target_range:
+						target_range = distance_vector.length()
+						output_direction = distance_vector.normalized()
+		
 	return Vector3(input_angle.x,0,input_angle.y) if output_direction == Vector3.ZERO else output_direction
 
 
